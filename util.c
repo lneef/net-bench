62cc6580e1695b970c5dd5c46feaa7c55afdca12
Lukas Neef <lukas.neef@tum.de>
2025-10-24 14:45:01 +0200

    receiver thread

--- util.c
+++ util.c
@@ -8,7 +8,7 @@ int launch_lcores(int (**lcore_fn)(void *), struct port_info *arg,
           rte_lcore_count() - 1);
   uint16_t i = 0, lcore_id;
   RTE_LCORE_FOREACH_WORKER(lcore_id) {
-    if (rte_eal_remote_launch(lcore_fn[i], arg, lcore_id) < 0) {
+    if (rte_eal_remote_launch(lcore_fn[i++], arg, lcore_id) < 0) {
       rte_log(RTE_LOG_ERR, RTE_LOGTYPE_USER1, "Failed to launch lcore %u\n",
               lcore_id);
       return -1;
